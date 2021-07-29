292c11d46aff4fd0d02efe6b108718ad4b64b299
Archishman <archishmant@gmail.com>
2021-07-29 14:05:25 +0000

    initial commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
 
 
 # -- Initialization section --
@@ -16,12 +16,12 @@ events = [
     ]
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
+app.config['MONGO_DBNAME'] = 'upperline'
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:WDNzZyXjJqvDE1dn@cluster0.9mmuk.mongodb.net/upperline?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
@@ -30,7 +30,8 @@ events = [
 @app.route('/index')
 
 def index():
-    return render_template('index.html', events = events)
+    user=mongo.db.users
+    return render_template('index.html', user = user)
 
 
 # CONNECT TO DB, ADD DATA
@@ -39,8 +40,27 @@ def index():
 
 def add():
     # connect to the database
-
+    user=mongo.db.users
     # insert new data
-
+    user.insert({'name': "Archishman"})
     # return a message to the user
-    return ""
+    return "Added user!"
+    
+
+@app.route('/events', methods = ["GET", "POST"])
+def events():
+    if request.method == "GET":
+        return "<h1>Try again!<h1>"
+    else:
+        user_name = request.form('first_name')
+        user_date = request.form('date')
+        
+        events = mongo.db.users
+        events.insert({'first_name': user_name, 'date': user_date})
+
+
+
+
+    # collection = mongo.db.users
+    # events = collection.find({})
+    # return render_template('events.html', users=users)
